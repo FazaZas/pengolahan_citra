334ea0a890b99cb09b99b80555f0dcf6115e9315
FazaZas <fazanur17@gmail.com>
2022-03-18 17:10:08 +0700

    refisi

--- P2/ocv2.py
+++ P2/ocv2.py
@@ -1,3 +1,5 @@
+from ast import Return
+from cv2 import COLOR_BGRA2BGR
 import numpy as np
 import cv2
 from matplotlib import pyplot as plt
@@ -5,47 +7,23 @@ import matplotlib.image as mpimg
 import time
 from array import *
 
-# Main Function
 
-
-def load_image():
-    img01 = cv2.imread('img/A1.jpg')
-    img02 = cv2.imread('img/A2.jpg')
-    img03 = cv2.imread('img/A3.jpg')
-    img04 = cv2.imread('img/A4.jpg')
-
-    plt.subplot(2, 2, 1), plt.imshow(img01)
-    plt.title('satu'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2, 2, 2), plt.imshow(img02)
-    plt.title('dua'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2, 2, 3), plt.imshow(img03)
-    plt.title('tiga'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2, 2, 4), plt.imshow(img04)
-    plt.title('empat'), plt.xticks([]), plt.yticks([])
-
-    plt.show()
-    plt.close()
-    return
-
-
-def load_image1():
-    img = cv2.imread('img/arsip.jpeg')
-    frame_resize = rescaleFrame(img, scale=5)
-    cv2.imshow('arsip', img)
-    cv2.imshow('arsip resize', frame_resize)
+def tampil_gambar():
+    img = cv2.imread('img/kuda.jpg')
+    cv2.imshow('img/kuda.jpg')
+    # Menunda windows terdestroy
     cv2.waitKey(0)
     return
 
 
 def access_image():
-    img01 = cv2.imread('img/bullish')
+    img01 = cv2.imread('img/bullish.jpeg')
     row1, col1, n = img01.shape
     print(row1, col1)
     img02 = np.zeros((row1, col1, 3), np.uint8)
-    img03 = np.zeros((140, 200, 3), np.uint8)
     img04 = np.zeros((140, 200, 3), np.uint8)
 
-    img02 = cv2.cvtColor(img01, cv2.COLOR_BGR2RGB)
+    img02 = cv2.cvtColor(img01, cv2, COLOR_BGRA2BGR)
     img03 = img02.copy()
 
     color = (0, 0, 255)
@@ -57,15 +35,13 @@ def access_image():
     plt.title('gambar 01'), plt.xticks([]), plt.yticks([])
     plt.subplot(2, 2, 2), plt.imshow(img02)
     plt.title('gambar 02'), plt.xticks([]), plt.yticks([])
-    plt.subplot(2, 2, 3), plt.imshow(img03)
+    plt.subplot(2, 2, 2), plt.imshow(img03)
     plt.title('gambar 03'), plt.xticks([]), plt.yticks([])
     plt.subplot(2, 2, 4), plt.imshow(img04)
     plt.title('gambar 04'), plt.xticks([]), plt.yticks([])
-    plt.show()
+
     return
 
 
-# ======Main Program=======
-# load_image()
-# load_image1()
-access_image()
+# access_image()
+tampil_gambar()
